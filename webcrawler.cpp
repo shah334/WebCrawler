4955c99520694d48de45ae38ebfaf1b4056f0298
Parshwa D Shah <shah334@purdue.edu>
2017-07-07 20:13:58 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -85,7 +85,7 @@ int main(int argc, char ** argv){
 		option = string(argv[1]);
 		maxUrls = atoi(argv[2]);
 		noUrls = argc - 3;
-		const char ** urls[noUrls];//Store the initial urls.
+		char ** urls[noUrls];//Store the initial urls.
 		
 		for(int i=3;i<noUrls;i++){
 			urls[k] = argv[i];
