71afa1cb9e92fa595d2ea43187c907ee9e11e58d
Parshwa D Shah <shah334@purdue.edu>
2017-07-08 18:51:58 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -51,7 +51,7 @@ WebCrawler::crawl()
 	 SimpleHTMLParser parser;
 	  bool temp = parser.parse(doc,len);//parse the document
 	  int t;
-	  _urlArray[_headURL] -> _description = strdup(parser.description);
+	  _urlArray[_headURL] -> _description = strdup(parser.description.c_str());
 	  for(int i=0;i<parser.urlvector.size();i++){
 		  _urlArray[_tailURL] -> _url = parser.urlvector[i];
 		  if(!_urlToUrlRecord->find(parser.urlvector[i].c_str(),&t)){
