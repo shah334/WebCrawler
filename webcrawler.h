f76c763afafb2fa349d1efd7ef22f813f2b5c453
Parshwa D Shah <shah334@purdue.edu>
2017-07-08 18:47:34 -0400

    Commit web-crawler

--- webcrawler.h
+++ webcrawler.h
@@ -7,8 +7,8 @@
 //using namespace std;
 // Stores a URL and the first 500 words
 struct URLRecord {
-  std::string _url;          // URL of the document.
-  std::string _description;  // First 500 words of the document without tags
+  char * _url;          // URL of the document.
+  char * _description;  // First 100 words of the document without tags
 };
 
 // Used to implement a list of URL indexes of the url array.
