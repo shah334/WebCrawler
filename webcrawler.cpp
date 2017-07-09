b956e1430cdd186d2f64e5db70d2ef41b0160d60
Parshwa D Shah <shah334@purdue.edu>
2017-07-08 20:28:46 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -54,12 +54,13 @@ WebCrawler::crawl()
 	  int t;
 	  _urlArray[_headURL]._description = strdup(parser.description.c_str());
 	  for(int i=0;i<parser.urlvector.size();i++){
-		 if(!_urlToUrlRecord->find(parser.urlvector[i].c_str(),&t)){
-			 _urlArray[_tailURL]._url = strdup(parser.urlvector[i].c_str());
-			 temp = _urlToUrlRecord->insertItem(_urlArray[_tailURL]._url,_tailURL);
-		  	 _tailURL ++;
-		  	 printf("%s\n",_urlArray[_tailURL]._url);
-		  }
+		 //if(!_urlToUrlRecord->find(parser.urlvector[i].c_str(),&t)){
+			// _urlArray[_tailURL]._url = strdup(parser.urlvector[i].c_str());
+			 //temp = _urlToUrlRecord->insertItem(_urlArray[_tailURL]._url,_tailURL);
+		  	 //_tailURL ++;
+		  	 //printf("%s\n",_urlArray[_tailURL]._url);
+		  //}
+	  	printf("%s",parser.urlvector[i].c_str());
 	  }
 	 _headURL++;
      /*Fetch the next URL in _headURL
