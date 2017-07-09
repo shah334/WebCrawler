5318464b5665d4feae8ca3d4d2e535fed6ac0803
Parshwa D Shah <shah334@purdue.edu>
2017-07-08 21:35:31 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -53,6 +53,7 @@ WebCrawler::crawl()
 	  bool temp = parser.parse(doc,len);//parse the document
 	  int t;
 	  _urlArray[_headURL]._description = strdup(parser.description.c_str());
+	  printf("%s\n",_urlArray[_headURL]._description);
 	  for(int i=0;i<parser.urlvector.size();i++){
 		 if(!_urlToUrlRecord->find(parser.urlvector[i].c_str(),&t)){
 			 _urlArray[_tailURL]._url = strdup(parser.urlvector[i].c_str());
@@ -71,7 +72,7 @@ WebCrawler::crawl()
         Find all the hyperlinks of this document and add them to the _urlArray and _urlToUrlRecord if they are not already in the _urlToUrlRecord. Only insert up to _maxURL entries.
 		For each word in the document without tags, add the index of this URL to a URLRecordList in the _wordToURLRecordList table if the URL is not already there.*/
  	}//while
- 	printf("%s\n",_urlArray[_headURL]._description);
+
 }
 
 int main(int argc, char ** argv){
