ceffed88bf3a16451103b94c53218d8bc8e179e7
Parshwa D Shah <shah334@purdue.edu>
2017-07-09 22:04:20 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -53,8 +53,8 @@ WebCrawler::crawl()
 	  bool temp = parser.parse(doc,len);//parse the document
 	  int t;
 	  _urlArray[_headURL]._description = strdup(parser.description.c_str());
-	  //printf("%s\n",_urlArray[_headURL]._description);
-	  printf("%s\n",parser.description.c_str());
+	  printf("%s\n",_urlArray[_headURL]._description);
+	  //printf("%s\n",parser.description.c_str());
 	  for(int i=0;i<parser.urlvector.size();i++){
 		 if(!_urlToUrlRecord->find(parser.urlvector[i].c_str(),&t)){
 			 _urlArray[_tailURL]._url = strdup(parser.urlvector[i].c_str());
